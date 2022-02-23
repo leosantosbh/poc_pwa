7af8e0264c4d43033d1a811547e688b65c301089
Leonardo Santos <leonardo.santos@rethink.dev>
2022-02-22 23:10:02 -0300

    adjust install app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+let deferredPrompt: any;
+
 function App() {
+
+  const [installable, setInstallable] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      deferredPrompt = e;
+      setInstallable(true);
+    })
+
+    window.addEventListener('appinstalled', () => {
+      alert('app installed');
+    })
+  }, []);
+
+  const handleInstall = () => {
+    setInstallable(false);
+    if (deferredPrompt && deferredPrompt.prompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then();
+    }
+  }
+
   return (
     <div className="select-method">
+      {installable && (
+        <button className="install-button" onClick={handleInstall}>
+          Add to Home Screen
+        </button>
+      )}
       <Link to="/online">
         <div className="select-method-on">Online</div>
       </Link>
